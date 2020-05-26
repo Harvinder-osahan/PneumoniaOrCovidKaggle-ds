3cca32c617a1175d309764af9766b0a93716a4a1
Harvinder Singh <46685955+Harvinder-osahan@users.noreply.github.com>
2020-05-27 01:16:45 +0530

    Update ChestXRay.py

--- ChestXRay.py
+++ ChestXRay.py
@@ -41,12 +41,12 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
-        'dataset/Pneumonia_and_COVID19/TRAIN/',
+        '/home/dataset/Pneumonia_and_COVID19/TRAIN/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        'dataset/Pneumonia_and_COVID19/TEST/',
+        '/home/dataset/Pneumonia_and_COVID19/TEST/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
