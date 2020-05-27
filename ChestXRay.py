bd34e4bb722af3e2afd2fb07487b388fdd8ce1d3
Harvinder Singh <46685955+Harvinder-osahan@users.noreply.github.com>
2020-05-27 11:52:15 +0530

    Model made more Accurate!

--- ChestXRay.py
+++ ChestXRay.py
@@ -18,9 +18,18 @@ model.add(Convolution2D(filters=32,
                        )
          )
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(filters=32,
+                        kernel_size=(3,3),
+                        activation='relu',
+                        input_shape=(64, 64, 3) 
+                       )
+         )
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 #Flatten layer
 model.add(Flatten())
 
+model.add(Dense(units=40, activation='relu'))
 
 model.add(Dense(units=40, activation='relu'))
 model.add(Dense(units=20, activation='relu'))
